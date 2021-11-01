19818559fe6f195078c925319e787045ed007c7a
Hugo Masclet <hugo@lukso.io>
2021-11-01 12:35:06 +0100

    Init repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>UP Sample React App</h1>
+      <p>Let's deploy and interact with a UP. This is a sample repo for the <a href="https://docs.lukso.tech/tools/getting-started">Getting Started</a> tutorial</p>
+    </div>
+  );
+}
+
+export default App;
