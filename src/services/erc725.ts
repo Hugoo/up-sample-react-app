770520ba9f5923ae804fc6d118e104a20f8c69c0
Hugo Masclet <hugo@lukso.io>
2021-11-15 15:15:04 +0100

    Update ERC725 Schema
    SupportedStandards:LSP3UniversalProfile

--- src/services/erc725.ts
+++ src/services/erc725.ts
@@ -7,10 +7,10 @@ export const fetchErc725Data = async (erc725ContractAddress: string) => {
   // https://github.com/lukso-network/LIPs/blob/master/LSPs/LSP-3-UniversalProfile.md
   const schema: ERC725JSONSchema[] = [
     {
-      name: 'SupportedStandards:ERC725Account',
-      key: '0xeafec4d89fa9619884b6b89135626455000000000000000000000000afdeb5d6',
+      name: 'SupportedStandards:LSP3UniversalProfile',
+      key: '0xeafec4d89fa9619884b6b89135626455000000000000000000000000abe425d6',
       keyType: 'Mapping',
-      valueContent: '0xafdeb5d6',
+      valueContent: '0xabe425d6',
       valueType: 'bytes',
     },
     {
