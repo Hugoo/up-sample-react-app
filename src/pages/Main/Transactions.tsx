af1d1696f71ef0602c9fae5acc2ca487e36fafdd
Hugo Masclet <hugo@lukso.io>
2021-12-15 15:51:29 +0100

    Simplify contract tx

--- src/pages/Main/Transactions.tsx
+++ src/pages/Main/Transactions.tsx
@@ -120,22 +120,20 @@ const Transaction: React.FC = () => {
       return;
     }
 
-    let erc725yContract = new web3.eth.Contract([defaultFunctions.setData]);
+    let erc725yContract = new web3.eth.Contract(
+      [defaultFunctions.setData],
+      '0x23a86EF830708204646abFE631cA1a60d04c4FbE',
+    );
 
-    const data = erc725yContract.methods
+    return erc725yContract.methods
       .setData(
         ['0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5'],
         [
           '0x6f357c6a70546a2accab18748420b63c63b5af4cf710848ae83afc0c51dd8ad17fb5e8b3697066733a2f2f516d65637247656a555156587057347a53393438704e76636e51724a314b69416f4d36626466725663575a736e35',
         ],
       )
-      .encodeABI();
-
-    return web3.eth
-      .sendTransaction({
+      .send({
         from: account,
-        to: '0x23a86EF830708204646abFE631cA1a60d04c4FbE',
-        data,
       })
       .once('sending', (payload) => {
         console.log(JSON.stringify(payload, null, 2));
